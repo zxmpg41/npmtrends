c7d91f147f41bc05cb66466863a6720d3ad9dc77
zxmpg41 <kellenberg.kevin@gmail.com>
2026-04-27 13:33:20 +0200

    remove theme-provider.tsx

--- src/components/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from './components/theme-provider'
+import { ThemeProvider } from 'next-themes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
